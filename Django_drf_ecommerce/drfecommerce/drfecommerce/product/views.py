8993c0a930d5b934d68a54286ab7f1035015b83f
parth5757 <psthakkar2@gmail.com>
2024-04-20 22:12:31 +0530

    new django_react added router work added

--- Django_drf_ecommerce/drfecommerce/drfecommerce/product/views.py
+++ Django_drf_ecommerce/drfecommerce/drfecommerce/product/views.py
@@ -1,9 +1,10 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 from rest_framework.response import Response
+from drf_spectacular.utils import extend_schema
 from .models import Category
 from .serializers import CategorySerializer
-
+    
 class CategoryViewSet(viewsets.ViewSet):
     """
     A simple viewsets for view all categories
@@ -11,6 +12,7 @@ class CategoryViewSet(viewsets.ViewSet):
 
     queryset = Category.objects.all()
 
+    @extend_schema(responses=CategorySerializer)
     def list(self, request):
         serializer = CategorySerializer(self.queryset, many=True)
         return Response(serializer.data)
--- Django_drf_ecommerce/drfecommerce/drfecommerce/settings/base.py
+++ Django_drf_ecommerce/drfecommerce/drfecommerce/settings/base.py
@@ -46,6 +46,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     # External Packages
     'rest_framework',
+    'drf_spectacular',
     # Internal App
     'drfecommerce.product',
 ]
@@ -121,4 +122,10 @@ STATIC_URL = 'static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-REST_FRAMEWORK = {}
+REST_FRAMEWORK = {
+    'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Django DRF Ecommerce",
+}
