f5a759b94a1cd0de75ac6459510e3993fb6447dc
parth5757 <psthakkar2@gmail.com>
2024-05-03 23:24:21 +0530

    django pytest test_example

--- Django_Pytest/test/test_ex1.py
+++ Django_Pytest/test/test_ex1.py
@@ -1,10 +1,20 @@
-import pytest
+# import pytest
 
-def test_example():
-    # here in commented code there is one pytest learning is that when we ise the wrong the before example it automatically detect and stop at there if you do this thing in next test than it stop at there 
-    # print("hello")
-    # assert 1 == 2
-    assert 1 == 1
+# function    Run once per test
+# class       Run once per class of tests
+#module       Run once per module
+# session     Run once per session
 
-def test_example1():
-    assert 1 == 1
+
+# # @pytest.mark.webtest
+# # @pytest.mark.skip
+# # @pytest.mark.xfail
+# # this three use for skip or ignore any test
+# def test_example():
+#     # here in commented code there is one pytest learning is that when we ise the wrong the before example it automatically detect and stop at there if you do this thing in next test than it stop at there 
+#     # print("hello")
+#     # assert 1 == 2
+#     assert 1 == 1
+
+# def test_example1():
+#     assert 1 == 1
